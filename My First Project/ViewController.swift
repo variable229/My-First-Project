840eb39c3499ef942330c50f95667ed143205f5e
Tri Le <tri.le@me.com>
2018-01-15 01:58:59 -0800

    Initial Commit

---
+++ My First Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First Project
+//
+//  Created by Tri Le on 1/15/18.
+//  Copyright © 2018 Tri Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
